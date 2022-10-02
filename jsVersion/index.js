cc471a69c24868a49d0d908aed36a0bb9b25dd29
Nathan Schwarz <nathan.schwarz97@gmail.com>
2022-10-02 22:05:05 +0200

    exporting almost done

--- jsVersion/index.js
+++ jsVersion/index.js
@@ -19,14 +19,31 @@ class IPhotoExporter {
       console.error(e);
     }
   }
-  async handleExport(folderName, album) {}
- 
-  startExport() {
+  async handleExport(folderName, album) {
+    const images = album.keyList.map(k => this.images[k])
+    const libraryName = this.library.split('/').pop()
+    const promises = images.map(i => {
+        let _promises = []
+        if (i.OriginalPath) {
+            const relativePath = i.OriginalPath.split(libraryName)[1]
+            const inPath = path.join(this.library, relativePath)
+            const outPath = path.join(folderName, i.Caption + '_original', path.extname(relativePath))
+            _promises.push(fs.copyFile(inPath, outPath))
+        }
+        const relativePath = i.ImagePath.split(libraryName)[1]
+        const inPath = path.join(this.library, relativePath)
+        const outPath = path.join(folderName, i.Caption, path.extname(relativePath))
+        _promises.push(fs.copyFile(inPath, outPath))
+        return Promise.all(_promises)
+    })
+    return Promise.all(promises)
+  }
+  async startExport() {
     this.albums.forEach((f) => {
       try {
         const folderName = path.join(
           this.output,
-          `${f.AlbumName}`.replace(new RegExp("/", "gi"), "-")
+          f.AlbumName.replace(new RegExp("/", "gi"), "-")
         );
         if (!fs.existsSync(folderName)) {
           if (f["Album Type"] === "Regular") {
@@ -39,15 +56,18 @@ class IPhotoExporter {
         console.error(e);
       }
     });
+    return Promise.all(this.promises)
   }
   constructor({ path, out }) {
     this.library = path;
     this.output = out;
     this.xml = this._getXML();
     this.albums = this.xml["List of Albums"];
+    this.count = this.albums.filter(f["Album Type"] === "Regular").reduce((r, i) => r + i.PhotoCount, 0)
+    this.images = this.xml["Master Image List"];
     this.promises = [];
     this.startExport();
-    //console.log(JSON.stringify(this.xml, null, 2))
+    //console.log(JSON.stringify(this.images, null, 2));
   }
 }
 
