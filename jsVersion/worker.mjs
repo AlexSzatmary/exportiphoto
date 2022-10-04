38c7ea0cacc86271f74d9953b296b88a97532703
Nathan Schwarz <nathan.schwarz97@gmail.com>
2022-10-05 01:13:47 +0200

    moved back to copy file && added ficlone flag for performances

--- jsVersion/worker.mjs
+++ jsVersion/worker.mjs
@@ -9,37 +9,46 @@ export default async function handleExport({
   images,
 }) {
   const chunks = chunk(images, 30);
-  try {
-    for (let c of chunks) {
-      let _promises = [];
-      for (let i of c) {
-        let modified = false;
-        if (i.OriginalPath) {
-          modified = true;
-          const relativePath = i.OriginalPath.substring(archivePath.length);
-          const inPath = path.join(libraryPath, relativePath);
-          const outPath = path.normalize(
-            path.join(folderName, i.Caption.replace(new RegExp("/", "gi"), "-") + path.extname(relativePath))
-          );
-          _promises.push(fs.promises.copyFile(inPath, outPath));
-        }
-        const relativePath = i.ImagePath.substring(archivePath.length);
-        const inPath = path.join(libraryPath, relativePath);
+  for (let c of chunks) {
+    let _promises = [];
+    for (let i of c) {
+      let modified = false;
+      if (i.OriginalPath) {
+        modified = true;
+        const relativePath = i.OriginalPath.substring(archivePath.length);
+        const inPath = path.normalize(path.join(libraryPath, relativePath));
         const outPath = path.normalize(
           path.join(
             folderName,
             i.Caption.replace(new RegExp("/", "gi"), "-") +
-              (modified ? "_modified" : "") +
               path.extname(relativePath)
           )
         );
-        _promises.push(fs.promises.copyFile(inPath, outPath));
+        if (fs.existsSync(inPath)) {
+          _promises.push(fs.promises.copyFile(inPath, outPath, fs.constants.COPYFILE_FICLONE));
+        }
       }
-      await Promise.all(_promises);
+      const relativePath = i.ImagePath.substring(archivePath.length);
+      const inPath = path.normalize(path.join(libraryPath, relativePath));
+      const outPath = path.normalize(
+        path.join(
+          folderName,
+          i.Caption.replace(new RegExp("/", "gi"), "-") +
+            (modified ? "_modified" : "") +
+            path.extname(relativePath)
+        )
+      );
+      if (fs.existsSync(inPath)) {
+        _promises.push(fs.promises.copyFile(inPath, outPath, fs.constants.COPYFILE_FICLONE));
+      }
+    }
+    if (_promises.length) {
+        try {
+            await Promise.all(_promises);
+        } catch (e) {
+            console.error(e)
+        }
       _promises = [];
     }
-  } catch (e) {
-    console.error(e);
-    throw e;
   }
 }
