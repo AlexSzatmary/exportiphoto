efd49b5fcde2279776c3f6b8f9dcb281cbf04b12
Nathan Schwarz <nathan.schwarz97@gmail.com>
2022-11-04 01:42:37 +0100

    should work

--- jsVersion/worker.mjs
+++ jsVersion/worker.mjs
@@ -1,5 +1,5 @@
 import path from "path";
-import fs from "fs";
+import fs, { stat } from "fs";
 import { execSync } from 'child_process'
 import sharp from 'sharp'
 
@@ -14,6 +14,7 @@ export default async function handleExport({
     outPath = outPath.replace(/\.png/g, '.jpg')
   }
   if (fs.existsSync(image.path)) {
+    const stats = fs.statSync(image.path)
     if (fs.existsSync(outPath)) {
       outPath = path.normalize(outPath.replace(path.extname(outPath), `-${image.id}${path.extname(outPath)}`))
     }
@@ -22,6 +23,7 @@ export default async function handleExport({
     } else {
       fs.renameSync(image.path, outPath)
     }
+    fs.utimesSync(outPath, stats.atime, stats.mtime)
   }
   execSync(`exiftool -overwrite_original -keywords= ${outPath}`)
   for (const keyword of image.keywords) {
